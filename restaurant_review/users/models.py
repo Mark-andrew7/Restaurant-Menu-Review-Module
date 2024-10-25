31d182d82d883dca546fa71f9c464281992f290b
Mark-andrew7 <kamaumark19@gmail.com>
2024-10-25 14:18:29 +0300

    Configured JWT authentication

--- restaurant_review/users/models.py
+++ restaurant_review/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    ADMIN = 'admin'
+    ASSISTANT = 'assistant'
+
+    ROLE_CHOICES = [
+        (ADMIN, 'Admin'),
+        (ASSISTANT, 'Assistant'),
+    ]
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=ASSISTANT)
